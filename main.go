4daef002bc678949e69f9d94e7a6f19ee8b85e6d
Gabriel MARIE <gaby01@youmail.fr>
2025-10-01 16:45:18 +0200

    Add URL handler functionality

--- main.go
+++ main.go
@@ -6,10 +6,16 @@ import (
 	"net/http"
 )
 
+func URLHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>URL visitée</h1><p>Vous avez visité : %s</p>", r.URL.Path)
+}
+
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "<h1>Site Web en Go</h1><p>Page d'accueil</p>")
 	})
+	
+	http.HandleFunc("/url", URLHandler)
 
 	fmt.Println("Serveur démarré sur http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
